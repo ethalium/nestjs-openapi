ce6bad58b9e79ea6d44ac98915e59c53e7f1829d
Marcel Mayerhofer <marcel.mayerhofer@gmail.com>
2026-08-27 01:55:46 +0200

    release(v0.5.0): adjusted @OASchema decorator to allow custom schema configuration

--- lib/interfaces/schema.interface.ts
+++ lib/interfaces/schema.interface.ts
@@ -1,3 +1,3 @@
-import type { ApiSchemaOptions } from '@nestjs/swagger';
+import type { ApiSchemaOptions, SchemaObject } from '@nestjs/swagger';
 
-export type IOpenApiSchemaOptions = ApiSchemaOptions;
+export type IOpenApiSchemaOptions = ApiSchemaOptions & SchemaObject;
